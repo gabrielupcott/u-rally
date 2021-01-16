10bae6aee2ff176594ba45b3eef49e2ac2393222
gabrielupcott <gabrielcupcott@gmail.com>
2021-01-16 15:34:46 -0500

    Redirected to Landing page by default and fixed routing to Create and Map pages.

--- u-rally/src/App.js
+++ u-rally/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-      <Route exact path="/Landing">
+      <Route exact path="/">
           <Landing />
         </Route>
         <Route exact path="/Create">
--- u-rally/src/Landing.js
+++ u-rally/src/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Link, BrowserRouter as Router, Switch, BrowserRouter} from 'react-router-dom'
 import './Landing.css'
 
 function Landing() {
@@ -9,7 +10,7 @@ function Landing() {
                 <div class="centered">
                     <h2>CREATE A MOVEMENT</h2>
                     <button class="button-left">
-                        CREATE
+                        <Link to="/Create">CREATE</Link>
                     </button>
                 </div>
             </div>
@@ -17,7 +18,7 @@ function Landing() {
                 <div class="centered">
                     <h2>FIND EVENTS</h2>
                     <button class="button-right">
-                       VIEW MAP
+                    <Link to="/Map">VIEW MAP</Link>
                     </button>
                 </div>
             </div>
